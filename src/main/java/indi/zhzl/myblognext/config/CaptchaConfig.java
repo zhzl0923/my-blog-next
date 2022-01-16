d09a4eef8391a907d41f4193c66367e82843735c
zzl <188877869@qq.com>
2022-01-16 15:16:50 +0800

    captcha image

---
+++ src/main/java/indi/zhzl/myblognext/config/CaptchaConfig.java
@@ -0,0 +1,115 @@
+package indi.zhzl.myblognext.config;
+
+import com.google.code.kaptcha.impl.DefaultKaptcha;
+import com.google.code.kaptcha.util.Config;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.Serializable;
+import java.util.Properties;
+
+@Configuration
+public class CaptchaConfig implements Serializable {
+
+    @Value("${captcha.font.size:'30'}")
+    private String fontSize;
+
+    @Value("${captcha.font.names:'宋体,楷体,微软雅黑'}")
+    private String fontNames;
+
+    @Value("${captcha.font.color:'red'}")
+    private String fontColor;
+
+    @Value("${captcha.image.width:'110'}")
+    private String captchaImageWidth;
+
+    @Value("${captcha.image.height:'40'}")
+    private String captchaImageHeight;
+
+    @Value("${captcha.session-key:'code'}")
+    private String sessionKey;
+
+    @Value("${captcha.char-length:'5'}")
+    private String charLength;
+
+    public String getFontColor() {
+        return fontColor;
+    }
+
+    public void setFontColor(String fontColor) {
+        this.fontColor = fontColor;
+    }
+
+    public String getCaptchaImageWidth() {
+        return captchaImageWidth;
+    }
+
+    public void setCaptchaImageWidth(String captchaImageWidth) {
+        this.captchaImageWidth = captchaImageWidth;
+    }
+
+    public String getCaptchaImageHeight() {
+        return captchaImageHeight;
+    }
+
+    public void setCaptchaImageHeight(String captchaImageHeight) {
+        this.captchaImageHeight = captchaImageHeight;
+    }
+
+    public String getFontSize() {
+        return fontSize;
+    }
+
+    public void setFontSize(String fontSize) {
+        this.fontSize = fontSize;
+    }
+
+    public String getSessionKey() {
+        return sessionKey;
+    }
+
+    public void setSessionKey(String sessionKey) {
+        this.sessionKey = sessionKey;
+    }
+
+    public String getCharLength() {
+        return charLength;
+    }
+
+    public void setCharLength(String charLength) {
+        this.charLength = charLength;
+    }
+
+    public String getFontNames() {
+        return fontNames;
+    }
+
+    public void setFontNames(String fontNames) {
+        this.fontNames = fontNames;
+    }
+
+    @Bean
+    public DefaultKaptcha getDefaultCaptcha() {
+        DefaultKaptcha dk = new DefaultKaptcha();
+        Properties properties = new Properties();
+        // 字体颜色
+        properties.setProperty("kaptcha.textproducer.font.color", this.getFontColor());
+        // 图片宽
+        properties.setProperty("kaptcha.image.width", this.getCaptchaImageWidth());
+        // 图片高
+        properties.setProperty("kaptcha.image.height", this.getCaptchaImageHeight());
+        // 字体大小
+        properties.setProperty("kaptcha.textproducer.font.size", this.getFontSize());
+        // session key
+        properties.setProperty("kaptcha.session.key", this.getSessionKey());
+        // 验证码长度
+        properties.setProperty("kaptcha.textproducer.char.length", this.getCharLength());
+        // 字体
+        properties.setProperty("kaptcha.textproducer.font.names", this.getFontNames());
+        Config config = new Config(properties);
+        dk.setConfig(config);
+
+        return dk;
+    }
+}
--- src/main/java/indi/zhzl/myblognext/controller/admin/LoginController.java
+++ src/main/java/indi/zhzl/myblognext/controller/admin/LoginController.java
@@ -1,9 +1,50 @@
 package indi.zhzl.myblognext.controller.admin;
 
+import com.google.code.kaptcha.impl.DefaultKaptcha;
+import indi.zhzl.myblognext.config.CaptchaConfig;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.imageio.ImageIO;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.OutputStream;
 
 @Controller
 @RequestMapping("/admin")
 public class LoginController {
+
+    @Autowired
+    private CaptchaConfig captchaConfig;
+    @Autowired
+    private DefaultKaptcha defaultKaptcha;
+
+    @GetMapping("/captcha")
+    public void captcha(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String captchaText = defaultKaptcha.createText();
+        BufferedImage image = defaultKaptcha.createImage(captchaText);
+        request.getSession().setAttribute(captchaConfig.getSessionKey(), captchaText);
+        OutputStream outputStream = null;
+        try {
+            outputStream = response.getOutputStream();
+            response.setContentType("image/jpeg");
+            response.setDateHeader("Expires", 0);
+            response.setHeader("Cache-Control", "no-store, no-cache, must-revalidate");
+            response.addHeader("Cache-Control", "post-check=0, pre-check=0");
+            response.setHeader("Pragma", "no-cache");
+            ImageIO.write(image, "jpeg", outputStream);
+        } catch (IOException e) {
+            System.out.println("获取图片异常");
+        } finally {
+            if (outputStream != null) {
+                outputStream.flush();
+                outputStream.close();
+            }
+
+        }
+    }
 }
